fac13e6b67e95001551323a6d1cd42e2d60c31ed
Florêncio Vieira Filho <florenciovieira@gmail.com>
2018-02-24 09:37:40 -0300

    Atualização

--- app/src/main/java/florencio/com/br/appperiodo/dominio/Ano.java
+++ app/src/main/java/florencio/com/br/appperiodo/dominio/Ano.java
@@ -5,23 +5,23 @@ import android.content.ContentValues;
 import florencio.com.br.appperiodo.util.Util;
 
 public class Ano extends Entidade {
-    final private Integer numero;
+	final private Integer numero;
 
-    public Ano(Integer numero) {
-        this.numero = numero;
-    }
+	public Ano(Integer numero) {
+		this.numero = numero;
+	}
 
-    public Integer getNumero() {
-        return numero;
-    }
+	public Integer getNumero() {
+		return numero;
+	}
 
-    public void processar() {
-        atual = numero == Util.ANO_ATUAL;
-    }
+	public void processar() {
+		atual = numero == Util.ANO_ATUAL;
+	}
 
-    public ContentValues criarContentValues() {
-        ContentValues cv = new ContentValues();
-        cv.put("numero", numero);
-        return cv;
-    }
+	public ContentValues criarContentValues() {
+		ContentValues cv = new ContentValues();
+		cv.put("numero", numero);
+		return cv;
+	}
 }
